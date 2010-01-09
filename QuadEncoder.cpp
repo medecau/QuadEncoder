9fb09a85795c42b9a65fec9bf68efba5d2ce68bc
medecau <medecau@gmail.com>
2010-01-09 18:26:41 +0000

    initial commit

---
+++ QuadEncoder.cpp
@@ -0,0 +1,79 @@
+/**
+ * QuadEncoder.cpp - Library for reading moves from a quadratic rotary encoder
+ * Created by Pedro Rodrigues (medecau@gmail.com) 9, January of 2010
+ * Released into the public domain.
+ */
+
+#include "WProgram.h"
+#include "QuadEncoder.h"
+
+QuadEncoder::QuadEncoder(int pin1, int pin2)
+{
+  pinMode(pin1, INPUT);
+  pinMode(pin2, INPUT);
+  digitalWrite(pin1, HIGH);
+  digitalWrite(pin2, HIGH);
+  _inputPin1=pin1;
+  _inputPin2=pin2;
+  _val1=1;
+  _val2=1;
+  _oldVal1=1;
+  _oldVal2=1;
+  _pos=0;
+  _oldPos=0;
+  _turn=0;
+  _turnCount=0;
+
+}
+
+char QuadEncoder::hb()
+{  
+  _val1 = digitalRead(_inputPin1);
+  _val2 = digitalRead(_inputPin2);
+    // Detect changes
+  if ( _val1 != _oldVal1 || _val2 != _oldVal2) {
+    _oldVal1=_val1;
+    _oldVal2=_val2;
+    
+      //for each pair there's a position out of four
+    if ( _val1 == 1 ) {// stationary position
+      if (_val2 == 1)
+        _pos = 0;
+      else if (_val2 == 0)
+        _pos = 3;
+    } else if (_val1 == 0){
+      if (_val2 == 1)
+        _pos = 1;
+      else if (_val2 == 0)
+        _pos = 2;
+    }
+    
+    _turn = _pos-_oldPos;
+    _oldPos = _pos;
+    if (abs(_turn) != 2) {
+      if (_turn == 1 || _turn == -3)
+        _turnCount++;
+      else if (_turn == -1 || _turn == 3)
+        _turnCount--;
+    }
+    if (_pos==0){
+      if (_turnCount>0){
+        _turnCount=0;
+        return '>';
+      } else if (_turnCount<0){
+        _turnCount=0;
+        return '<';
+      } else {
+        _turnCount=0;
+        return '-';
+      }
+    }
+    
+    
+    
+    
+    
+    
+    
+  }
+}
---
+++ QuadEncoder.h
@@ -0,0 +1,31 @@
+/**
+ * QuadEncoder.h - Library for reading moves from a quadratic rotary encoder
+ * Created by Pedro Rodrigues (medecau@gmail.com) 9, January of 2010
+ * Released into the public domain.
+ */
+
+#ifndef QuadEncoder_h
+#define QuadEncoder_h
+
+#include "WProgram.h"
+
+
+class QuadEncoder
+{
+  public:
+    QuadEncoder(int pin1, int pin2);
+    char hb();
+  private:
+    int _inputPin1;
+    int _inputPin2;
+    int _val1;
+    int _val2;
+    int _oldVal1;
+    int _oldVal2;
+    int _pos;
+    int _oldPos;
+    int _turn;
+    int _turnCount;  
+  };
+
+#endif
