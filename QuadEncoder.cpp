29d84a4cb7de9d87ed447c664b2291e2dee3061b
alekas <spadusa@gmail.com>
2010-01-31 21:50:49 -0700

    Changed hb function to readEncoder. Added support for the shaft button press, added a moved() boolean function to easily check if the encoder was rotated

--- QuadEncoder.cpp
+++ QuadEncoder.cpp
@@ -1,20 +1,25 @@
 /**
  * QuadEncoder.cpp - Library for reading moves from a quadratic rotary encoder
  * Created by Pedro Rodrigues (medecau@gmail.com) 9, January of 2010
+ * Modified by Andreas Lekas (spadusa@gmail.com) 31, January, 2010
  * Released into the public domain.
  */
 
 #include "WProgram.h"
 #include "QuadEncoder.h"
 
-QuadEncoder::QuadEncoder(int pin1, int pin2)
+QuadEncoder::QuadEncoder(int pin1, int pin2, int pin3)
 {
   pinMode(pin1, INPUT);
   pinMode(pin2, INPUT);
+  pinMode(pin3, INPUT);
   digitalWrite(pin1, HIGH);
   digitalWrite(pin2, HIGH);
+  digitalWrite(pin3, HIGH);
+  _moved = false;
   _inputPin1=pin1;
   _inputPin2=pin2;
+  _inputPin3=pin3;
   _val1=1;
   _val2=1;
   _oldVal1=1;
@@ -23,11 +28,11 @@ QuadEncoder::QuadEncoder(int pin1, int pin2)
   _oldPos=0;
   _turn=0;
   _turnCount=0;
-
 }
 
-char QuadEncoder::hb()
+char QuadEncoder::readEncoder()
 {  
+  _moved = false;
   _val1 = digitalRead(_inputPin1);
   _val2 = digitalRead(_inputPin2);
     // Detect changes
@@ -59,21 +64,26 @@ char QuadEncoder::hb()
     if (_pos==0){
       if (_turnCount>0){
         _turnCount=0;
+		_moved = true;
         return '>';
       } else if (_turnCount<0){
+		_moved = true;
         _turnCount=0;
         return '<';
       } else {
+		_moved = false;
         _turnCount=0;
         return '-';
       }
     }
-    
-    
-    
-    
-    
-    
-    
   }
+}
+bool QuadEncoder::didMove()
+{
+	return _moved;
+}
+
+bool QuadEncoder::pressed()
+{
+	return digitalRead(_inputPin3)==0;
 }
